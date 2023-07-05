06fc86f44d8d0b3c2a448aab5206ecf1f21ebe82
taabroeders <tommybroeders@gmail.com>
2023-07-05 17:43:34 +0200

    init bug fixes

--- init.bash
+++ init.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-[ -f $0/files/ICA-AROMA/ICA_AROMA.py ] || git submodule update --init --recursive
+[ -f $(dirname $0)/files/ICA-AROMA/ICA_AROMA.py ] || git submodule update --init --recursive
 
-[ -f $0/files/singularity/synbold-disco.sif] || singularity pull $0/files/singularity/synbold-disco.sif docker://ytzero/synbold-disco:v1.4
+[ -f $(dirname $0)/files/singularity/synbold-disco.sif ] || singularity pull $(dirname $0)/files/singularity/synbold-disco.sif docker://ytzero/synbold-disco:v1.4
 
-[ -f $0/files/singularity/synb0-disco.sif ] || singularity pull $0/files/singularity/synb0-disco.sif docker://leonyichencai/synb0-disco:v3.0
+[ -f $(dirname $0)/files/singularity/synb0-disco.sif ] || singularity pull $(dirname $0)/files/singularity/synb0-disco.sif docker://leonyichencai/synb0-disco:v3.0
 
-[ -d $0/files/preproc_env ] || conda env create --prefix $0/files/preproc_env -f $0/files/environment.yml
+[ -d $(dirname $0)/files/preproc_env ] || conda env create --prefix $(dirname $0)/files/preproc_env -f $(dirname $0)/files/environment.yml
