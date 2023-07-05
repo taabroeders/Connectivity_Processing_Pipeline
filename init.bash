85512b3907ed10509331219c100c5840ed1a7c0f
taabroeders <tommybroeders@gmail.com>
2023-07-05 16:21:50 +0200

    actual removal of folder

--- init.bash
+++ init.bash
@@ -5,3 +5,5 @@
 [ -f $0/files/singularity/synbold-disco.sif] || singularity pull $0/files/singularity/synbold-disco.sif docker://ytzero/synbold-disco:v1.4
 
 [ -f $0/files/singularity/synb0-disco.sif ] || singularity pull $0/files/singularity/synb0-disco.sif docker://leonyichencai/synb0-disco:v3.0
+
+[ -d $0/files/preproc_env ] || conda env create --prefix $0/files/preproc_env -f $0/files/environment.yml
