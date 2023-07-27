c06ce1f0d73c96a7400788d810f943aa1a08bcd0
taabroeders <tommybroeders@gmail.com>
2023-07-27 16:28:11 +0200

    include filling code in init script

--- init.bash
+++ init.bash
@@ -4,6 +4,8 @@ module load Anaconda3/2023.03
 
 [ -f $(dirname $0)/files/ICA-AROMA/ICA_AROMA.py ] || git submodule update --init --recursive
 
+[ -f $(dirname $0)/files/niftyseg/bin/seg_FillLesions ] || git submodule update --init --recursive
+
 [ -f $(dirname $0)/files/singularity/synbold-disco.sif ] || singularity pull $(dirname $0)/files/singularity/synbold-disco.sif docker://ytzero/synbold-disco:v1.4
 
 [ -f $(dirname $0)/files/singularity/synb0-disco.sif ] || singularity pull $(dirname $0)/files/singularity/synb0-disco.sif docker://leonyichencai/synb0-disco:v3.0
